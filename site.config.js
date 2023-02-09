5a6299da3a68cbd4d2f1aaa3a5ad5dad2be8a69c
Kevin van den Burg <kevin@kodea.nl>
2023-02-09 17:28:36 +0100

    change website url

--- site.config.js
+++ site.config.js
@@ -1,12 +1,13 @@
 export const siteConfig = {
   titleTemplate: 'Shiba Saga', // Site default title. <titleTemplate> - <title>
   title: '$SHIA The first meme coin to offer a complete Saga of games', // Default title attached to the template if none is provided.
-  description: '$SHIA will be the first MEME coin (with utility) to offer a complete Saga of games that share the same token. A collection of rebranded casual games that people already know how to play and love, done the Shiba way.  meta description there', // Website description used for meta description tag.
+  description:
+    '$SHIA will be the first MEME coin (with utility) to offer a complete Saga of games that share the same token. A collection of rebranded casual games that people already know how to play and love, done the Shiba way.  meta description there', // Website description used for meta description tag.
   author: 'Shiba Saga', // Author of the website.
   twitter: '@author', // Twitter of the author.
   lang: 'en',
   image: 'ogimage.jpg',
-  url: 'https://www.shibasaga.com', // Domain of your website without pathPrefix.
+  url: 'https://shiba-saga.io', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/sub-folder/.
   themeColor: '#1F1E29', // Used for setting manifest and progress theme colors.
   backgroundColor: '#1F1E29', // Used for setting manifest background color.
